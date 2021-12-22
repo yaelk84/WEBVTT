b01d8091554a28331b5ae78059003dbeaa152d2c
yaelk84 <yaelk@realcommerce.co.il>
2021-12-22 09:08:41 +0200

    ALL

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -30,42 +30,40 @@ export class HomeComponent implements OnInit {
       forkJoin([this.server.getBridgeTx(), this.server.getCharacters()]).subscribe((results) => {
           this.data = this.dataService.getFormattedData(results[1],results[0]);
           this.render = true;
-      });
+          const date1 = Date.now();
+        this.zone.runOutsideAngular(() => {
+          requestAnimationFrame(() => {
 
-    const date1 = Date.now();
+            this.countDown =timer(0, this.tick).subscribe(()=>{
+              // update currentTime
+              this.counter = this.getCurrentTime(date1);
+              // iterate all items on screen and calc time pass for each cube
+              if (this.scroll && this.scroll.viewPortItems) {
+                this.scroll.viewPortItems.forEach((item: IvvtDisplay) => {
+                  item.heightByTime = this.calcHeight(item.start, item.end);
+                })
+              }
+              this.ref.detectChanges();
 
-    this.zone.runOutsideAngular(() => {
-      requestAnimationFrame(() => {
 
-        this.countDown =timer(0, this.tick).subscribe(()=>{
-
-          const date2 = Date.now();
-          const diffTime = Math.abs(+date2 - +date1);
-          if (diffTime > 10000){
-            this.countDown.unsubscribe()
-          }
-          this.counter = diffTime;
-
-          console.count();
-
-          if (this.scroll && this.scroll.viewPortItems) {
-            this.scroll.viewPortItems.forEach((item: IvvtDisplay) => {
-              item.heightByTime = this.calcHeight(item.start, item.end);
             })
-          }
-          this.ref.detectChanges()
+          });
+        });
 
-        })
+
+
       });
-    });
 
 
+  }
 
-
-
-
-
-
+  getCurrentTime(startDate):number{
+    const date2 = Date.now();
+    const diffTime = Math.abs(+date2 - +startDate);
+    if (diffTime > this.data[this.data.length - 1].end){
+      this.countDown.unsubscribe()
+    }
+    return  diffTime;
   }
   calcHeight(start:number, end:number): string {
 
